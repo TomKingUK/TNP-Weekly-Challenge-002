8e0e36de42d9055f6f005a85fdde710b3a573b97
TomKingUK <59918804+TomKingUK@users.noreply.github.com>
2021-01-14 12:47:11 +0000

    Update test_message.py

--- python-project/test_message.py
+++ python-project/test_message.py
@@ -13,13 +13,12 @@ def fail():
 def test_decode_message():
     try:
         message = decode_message(['.--.', '-.--', '-', '....', '---', '-.', '/', '..', '...', '/', '..-.', '..-', '-.', '-.-.--'])
-        assert message == 'python is fun!', 'Incorrect answer.'
+        assert message == 'python is fun!', f'Incorrect answer:\n\n{message}.'
         success()
         send_msg('🌟 Congratulations, you decoded the message:', f'Message: {message}')
 
     except AssertionError as e:
         fail()
-        send_msg('Output:', message)
         send_msg("Oops, something's not right! 🐞", e)
         send_msg("Hint 💡", "Did you remember to return the message as a single lower-case string? 🤔")
         send_msg("Hint 💡", "Did you rememeber to include spaces and punctuation? 🤔")
