75c3a9c2f46cfcb5e475a01b45efb3bd874fac7a
TomKingUK <59918804+TomKingUK@users.noreply.github.com>
2021-01-14 12:17:05 +0000

    Update message.py

--- python-project/message.py
+++ python-project/message.py
@@ -1,5 +1,5 @@
-def count_all_stars(galaxies):
-    total_stars = 0
-    for stars in galaxies:
-        total_stars = stars  # fix me!
-    return total_stars
+def decode_message(message):
+    # Decode the message
+    
+    
+    print(message)
